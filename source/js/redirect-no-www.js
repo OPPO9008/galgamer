991106ae510b0a9095655e2c9875ba0f6e1a3582
桐遠暮羽 <ymbk@github.com>
2021-11-03 19:34:01 +0800

    增加從 http 到 https，從 www 到非 www 的重定向

---
+++ source/js/redirect-no-www.js
@@ -0,0 +1,39 @@
+function main() {
+    redirectHttps();
+    redirectNoWWW();
+}
+
+function redirectHttps() {
+    if(window.location.protocol == 'http:'){
+        if(window.location.port){
+            log("當前網址有端口，可能處於調試模式，跳過 HTTPS 重定向。")
+            return;
+        }
+        console.log("You are visiting from insecure HTTP, redirecting")
+        let newLocation = 'https://' + window.location.host + window.location.pathname;
+        window.location = newLocation;
+    }
+}
+
+function redirectNoWWW() {
+    if(window.location.host == 'www.galgamer.xyz'){
+        log('帶 www 的網址無法載入留言區，需要重定向。');
+        let newLocation = 'https://galgamer.xyz' + window.location.pathname;
+        window.location = newLocation;
+        return;
+    }
+    if(window.location.host != 'galgamer.xyz'){
+        log('This URL host seems not belong to the official website.');
+    }
+}
+
+function log(text) {
+    let currentdate = new Date(); 
+    let datetime = "[" +  
+                + currentdate.getHours() + ":"  
+                + currentdate.getMinutes() + ":" 
+                + currentdate.getSeconds() + "] ";
+    console.log('[RedirectJS]' + datetime + text);
+}
+
+main();
