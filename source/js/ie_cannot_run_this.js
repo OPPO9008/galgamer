3431a64cccb93d14b78f97bc058a96ed3cfe4a29
桐遠暮羽 <ymbk@github.com>
2021-11-23 21:29:30 +0800

    China CDN test

--- source/js/ie_cannot_run_this.js
+++ source/js/ie_cannot_run_this.js
@@ -7,16 +7,45 @@ async function checkChina () {
     .catch(error => log(error))
     
     if(country === 'CN'){
-        log('China!');
+        //log('China!');
         return true;
     }else{
-        log('Not China!');
+        //log('Not China!');
         return false;
     }
 }
 
-function chinaCDN(){
+async function chinaCDN(){
     // 中國 CDN 加速...
+    if(await checkChina()){
+        
+        let needCDN = document.querySelectorAll('[src-cn]');
+        
+        needCDN.forEach(function(tag){
+            log('China CDN for');
+            log(tag);
+            
+            fetch(tag.getAttribute('src-cn') ,{ method: "HEAD" })
+            .then(function(resp){
+                if(resp.ok){
+                    let size = resp.headers.get('content-length');
+                    log('China CDN for ' + tag.getAttribute('src') + 'seems ok, size: ' + size);
+                    // do some replace src stuff
+                    tag.setAttribute('src-origin', tag.getAttribute('src'))
+                    tag.setAttribute('src', tag.getAttribute('src-cn'));
+                    // done!
+                }else{
+                    //something goes wrone, skip this tag
+                    log('China CDN for ' + tag.getAttribute('src') + 'error, resp not ok.');
+                }
+            })
+            .catch(function(error){
+                log('China CDN for ' + tag.getAttribute('src') + 'error.');
+                log(error);
+            })
+            
+        })
+    }
 }
 
 chinaCDN();
