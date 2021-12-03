9bf750d8b6fd8d98338654eb5cff4832c2c27e74
桐遠暮羽 <ymbk@github.com>
2021-12-03 14:03:11 +0800

    comment improve

--- source/js/article_custom.js
+++ source/js/article_custom.js
@@ -1,5 +1,5 @@
 let loadingMsg = document.getElementById('loadingMsg');
-
+let insertDiv = document.getElementById('commentsiframe');
 let failedCounts = 0;
 let myInterval;
 
@@ -18,3 +18,26 @@ myInterval = setInterval(function() {
         return;
     }
 }, 50);
+
+function getDark(){
+    let mhtml = document.querySelectorAll('[data-user-color-scheme]')[0];
+    let result = mhtml.getAttribute('data-user-color-scheme');
+    return result === 'dark';
+}
+
+//insert script here
+let comment_script = document.createElement('script');
+comment_script.setAttribute('src','https://comments.app/js/widget.js?3');
+comment_script.setAttribute('data-comments-app-website', 's85yRa-y');
+comment_script.setAttribute('data-limit', '20');
+comment_script.setAttribute('data-color', 'E22F38');
+comment_script.setAttribute('data-dislikes', '1');
+comment_script.setAttribute('data-colorful', '1');
+if(getDark()){
+    comment_script.setAttribute('data-dark', '1');
+}
+insertDiv.appendChild(comment_script);
+/* 
+<script async src="https://comments.app/js/widget.js?3" data-comments-app-website="s85yRa-y" data-limit="20" data-color="E22F38" data-dislikes="1" data-colorful="1">
+</script>
+*/
