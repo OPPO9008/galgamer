35f5cb33757ec80e3eb0cb263a3b7797b6e40ce3
桐遠暮羽 <ymbk@github.com>
2021-12-06 19:45:54 +0800

    implement toast

--- source/js/mylib_async.js
+++ source/js/mylib_async.js
@@ -10,10 +10,10 @@ function main(){
 function redirectHttps() {
     if(window.location.protocol == 'http:'){
         if(window.location.port){
-            log("當前網址有端口，可能處於調試模式，跳過 HTTPS 重定向。")
+            mlog("當前網址有端口，可能處於調試模式，跳過 HTTPS 重定向。")
             return;
         }
-        log("You are visiting from insecure HTTP, redirecting")
+        mlog("You are visiting from insecure HTTP, redirecting")
         let newLocation = 'https://' + window.location.host + window.location.pathname;
         window.location = newLocation;
     }
@@ -21,23 +21,23 @@ function redirectHttps() {
 
 function redirectNoWWW() {
     if(window.location.host == 'www.galgamer.xyz'){
-        log('帶 www 的網址無法載入留言區，需要重定向。');
+        mlog('帶 www 的網址無法載入留言區，需要重定向。');
         let newLocation = 'https://galgamer.xyz' + window.location.pathname;
         window.location = newLocation;
         return;
     }
     if(window.location.host != 'galgamer.xyz'){
-        log('This URL host seems not belong to the official website.');
+        mlog('This URL host seems not belong to the official website.');
     }
 }
 
-function log(text) {
+function mlog(text) {
     let currentdate = new Date(); 
     let datetime = "[" +  
                 + currentdate.getHours() + ":"  
                 + currentdate.getMinutes() + ":" 
                 + currentdate.getSeconds() + "] ";
-    console.log('[Log]' + datetime + text);
+    console.log('[mylib]' + datetime + text);
 }
 
 function getDark(){
@@ -56,7 +56,7 @@ function checkMinSize(size){
         let test = width;
     }
     test = test * window.devicePixelRatio;
-    Log('The screen size is ' + test);
+    mlog('The screen size is ' + test);
     return test > size;
 }
 
@@ -92,6 +92,44 @@ function makeAlert(type, html, forceTop){
     if(forceTop){
         setTimeout(function(){window.scrollTo(0, 0);}, 300);
     }
+}
+
+function removeFadeOut( el, speed ) {
+    var seconds = speed/1000;
+    el.style.transition = "opacity "+seconds+"s ease";
+
+    el.style.opacity = 0;
+    setTimeout(function() {
+        el.parentNode.removeChild(el);
+    }, speed);
+}
+
+async function insertToast(type, data, last){
+    //<div id="mytoast" class="fixed-bottom bg-success text-light col-lg-4 col-md-10 col-sm-9 w-75 mx-auto py-2 my-2 rounded-lg">testtesttest</div>
+    let old = document.getElementById('mytoast');
+
+    if(old){
+        removeFadeOut(old, 500);
+        deley = 500;
+        // 等舊的消失
+        await new Promise(r => setTimeout(r, 500));
+    }
+
+    let toast = document.createElement('div')
+    toast.setAttribute('id', 'mytoast');
+    //toast.setAttribute('class', 'border border-light fixed-bottom text-light col-lg-4 col-md-10 col-sm-9 w-75 mx-auto py-2 my-2 rounded-lg' + ' bg-' + type);
+    toast.setAttribute('class', 'alert border border-primary fixed-bottom col-lg-4 col-md-10 col-sm-9 w-75 mx-auto py-2 mb-5 my-2 rounded-lg' + ' alert-' + type);
+    toast.innerHTML = data;
+    // 漸變進入
+    toast.style.transition = "opacity 0.5s ease";
+    toast.style.opacity = 0;
+    document.body.appendChild(toast);
+    await new Promise(r => setTimeout(r, 0));
+    toast.style.opacity = 100;
+    // 消失
+    setTimeout(function() {
+        removeFadeOut(toast, 500);
+    }, last);
 }
 
 main();
