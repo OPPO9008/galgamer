34fcb58c913dbc3c3c06f8c2c3184d1a33c5f2bb
桐遠暮羽 <ymbk@github.com>
2022-02-11 10:58:22 +0800

    improve api and robots.txt

--- functions/api/music/[id].js
+++ functions/api/music/[id].js
@@ -8,33 +8,41 @@ export async function onRequest(context) {
     next, // used for middleware or to fetch assets
     data, // arbitrary space for passing data between middlewares
   } = context;
-  
-  let pathArray = request.url.split( '/' );
+
+  let pathArray = request.url.split('/');
   let protocol = pathArray[0];
   let host = pathArray[2];
   let origin = protocol + '//' + host;
 
   let id = parseInt(params.id);
-  if(id >= 0){
+  if (id >= 0) {
     // 歌曲清單
     let list = await fetch(origin + '/music/music.json').then(resp => resp.json());
-    
+
     // 確認 ID 是否合法
-    if(id >= list.length)return new Response('ID 超出', {status: 400});
+    if (id >= list.length)
+      return new Response('<p>ID 超出</p>', {
+        status: 404,
+        headers: {
+          "content-type": "text/html;charset=UTF-8",
+          "cache-control": "public; max-age=43200",
+          "x-robots-tag": "noindex"
+        }
+      });
 
     // 開始進行所需的元素替換
     //let page = await fetch(murl);
     //let parser = new DOMParser();
     //let doc = parser.parseFromString(page, 'text/html');
 
-    // 所需的信息 
+    // 所需的信息
     let songTitle = list[id]['name'];
     let songArtist = list[id]['artist'];
     let songDetails = list[id]['details'];
     let songPoster = list[id]['poster'];
 
     let redirectTo = origin + '/music/?id=' + id;
-    
+
     let html = `
       <!DOCTYPE html>
       <head>
@@ -56,18 +64,26 @@ export async function onRequest(context) {
         <h1>` + songTitle + ` - ` + songArtist + `</h1>
         <h2>Loading...</h2>
         <script>
-          window.location = '`+ redirectTo +`';
+          window.location = '` + redirectTo + `';
         </script> 
       </body>
     `
-    return new Response(html, {
-    headers: {
-      "content-type": "text/html;charset=UTF-8",
-      "cache-control": "public; max-age=43200"
-    }
-  });
-  }else{
-    return new Response('ID 過於惡俗', {status: 400});
+      return new Response(html, {
+        headers: {
+          "content-type": "text/html;charset=UTF-8",
+          "cache-control": "public; max-age=43200",
+          "x-robots-tag": "noindex"
+        }
+      });
+  } else {
+    return new Response('<p>ID 過於惡俗</p>', {
+      status: 404,
+      headers: {
+        "content-type": "text/html;charset=UTF-8",
+        "cache-control": "public; max-age=43200",
+        "x-robots-tag": "noindex"
+      }
+    });
   }
-  
+
 }
